b829712dd9d33224353431f1db4f712eeefc7e30
gus <gussiciliano@gmail.com>
2024-07-02 18:02:51 -0300

    wip

---
+++ fullstack/src/main/java/com/unla/eventos/entities/Event.java
@@ -0,0 +1,41 @@
+package com.unla.grupo3.entities;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String name;
+	
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
+
+    private String publicFormLink;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
