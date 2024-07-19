a9f57a1c7df00dc3e4ed48d33b3b20bd2049231a
Gustavo Siciliano <gussiciliano@gmail.com>
2024-07-19 13:48:09 -0300

    fixes-on-more-urls

--- fullstack/src/main/java/com/unla/eventos/helpers/PublicLinksHelper.java
+++ fullstack/src/main/java/com/unla/eventos/helpers/PublicLinksHelper.java
@@ -1,6 +1,6 @@
 package com.unla.eventos.helpers;
 
 public class PublicLinksHelper {
-	public final static String PUBLIC_REGISTER_LINK = "/registro/";
-	public final static String PUBLIC_EVENT_RESPONSES = "/responses/list/";
+	public final static String PUBLIC_REGISTER_LINK = "registro/";
+	public final static String PUBLIC_EVENT_RESPONSES = "responses/list/";
 }
